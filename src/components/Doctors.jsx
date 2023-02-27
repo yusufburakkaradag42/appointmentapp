164ad9a64a58c8075fc90dd564170d59ceb1e8d3
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-27 22:41:28 +0300

    list

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,24 +1,55 @@
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import { AddModal } from "./AddModal"
+import { useState } from "react"
 
-const Doctors = ({ doctors }) => {
-  
+const Doctors = ({ doctors , appointment,setAppointmend}) => {
+  const [show, setShow] = useState(false)
+  const [selectedDrName, setSelectedDrName] = useState("")
+
+  // const handleClose = () => setShow(false)
+  // const handleShow = () => setShow(true)
+
+  const handleClick = (drName) => {
+    // handleShow()
+    setShow(true)
+    setSelectedDrName(drName)
+  }
+
+  console.log(selectedDrName)
   return (
     <Container className="p-2">
-      <h3 className="display-6" style={{ color: "rgb(166, 18, 189)" }}>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
 
       <Row className="justify-content-center">
         {doctors.map((dr) => (
-          <Col key={dr.id} xs={6} sm={4} md={3} lg={2}>
-            <img src={dr.img} alt={dr.name}  className="img-thumbnail doctor-img"/>
-            <h4>{dr.name}</h4>
+          <Col key={dr.id} 
+          xs={6}
+          sm={4} 
+          md={3}>
+            <img
+              src={dr.img}
+              alt={dr.name}
+              className="img-thumbnail doctor-img"
+              onClick={() => handleClick(dr.name)}
+            />
+            <h5>{dr.name}</h5>
             <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
+      {/* <AddModal show={show} handleClose={handleClose} /> */}
+      <AddModal
+        show={show}
+        handleClose={() => setShow(false)}
+        drName={selectedDrName}
+        appointment={appointment}
+        setAppointmend={setAppointmend}
+/>
+    
     </Container>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,26 @@ import { useState } from "react"
 import AppointmentList from "../components/AppointmentList"
 import Doctors from "../components/Doctors"
 import { doctorData } from "../helper/data"
+import { appointmentData } from "../helper/data"
 
 const Home = () => {
   const [doctors, setDoctors] = useState(doctorData)
+  const [appointment, setAppointmend] = useState(appointmentData)
+
+ 
+    
+  
 
   console.log(doctors)
   return (
     <main className="text-center mt-2 vh-100">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
-      <Doctors doctors={doctors} />
+      <Doctors doctors={doctors} 
+      appointment={appointment}
+      setappointmend={setAppointmend}/>
       <AppointmentList />
     </main>
   )
-}
+  }
 
 export default Home
