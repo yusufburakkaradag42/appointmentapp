f0e4c8c5561668db2434426e210a2e5ee3dc9258
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-27 20:56:51 +0300

    docters modul

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -10,12 +10,12 @@ const Doctors = ({ doctors }) => {
         Our Doctors
       </h3>
 
-      <Row>
+      <Row className="justify-content-center">
         {doctors.map((dr) => (
           <Col key={dr.id} xs={6} sm={4} md={3} lg={2}>
-            <img src={dr.img} alt={dr.name}  className="img-thumbnail"/>
+            <img src={dr.img} alt={dr.name}  className="img-thumbnail doctor-img"/>
             <h4>{dr.name}</h4>
-            <h5>{dr.dep}</h5>
+            <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
