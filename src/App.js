271cba4b91eb2945f18a484d3add8231c7ac1126
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-27 20:51:22 +0300

    doctors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
+import "./App.css"
+import Home from "./pages/Home"
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+const App = () => {
+  return <Home />
 }
 
-export default App;
+export default App
